7e72a957ad06fa2dfa7ad15bd58610a129e24d0f
miakm <mmarkeset@gmail.com>
2023-03-30 13:54:03 +0200

    endringer

---
+++ diagrammer/diagrammer.md
@@ -0,0 +1,14 @@
+# Objektorienterte modeller som beskriver programstruktur
+
+## UML-diagram
+Vi bruker UML diagram for å planlegge prosjekter innenfor objektorienterte programmer. Man lager en oversikt over hvilke klasser man skal bruke, og deretter gir man klassene egenskaper og metoder som skal brukes i klassen. 
+
+### Eksempel:
+![](uml.png)
+
+# Representasjon av programflyt med kontrollstrukturer
+
+## FlytdiagramVi bruker flytdiagram for å illustrere hvordan et program skal oppføre seg. Brukeren tar et valg, og programmet handler etter dette valget. Man bruker flytdiagram for å bedre planlegge programmene sine. 
+
+### Eksempel:
+![](flytdiagram.png)
--- pygame-23mars/oppsett.py
+++ pygame-23mars/oppsett.py
@@ -1,18 +1,22 @@
 
-
-# Example file showing a basic pygame "game loop"
 import pygame
 from random import randint
 import time
 
-# pygame setup
+
+pygame.init()
+pygame.font.init()
+
+
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
 biden = pygame.image.load("joe.png")
+biden = pygame.transform.scale(biden, (140,100))
 iskrem = pygame.image.load("ice.png")
+iskrem = pygame.transform.scale(iskrem, (100, 100))
 
 
 class Figur:
@@ -29,53 +33,60 @@ class Spiller(Figur):
         self._fartY = fartY
     
     def hopp(self):
-        self._y -= 10
-        self._x -= 4
+        if 0 <= self._y:
+            self._y -= 10
+
+    def side_h(self):
+        if self._x < 1280-140:
+            self._x += 10
+
+    def side_v(self):
+        if 0 <= self._x:
+            self._x -= 10
 
     def fall(self):
-        self._y += 4
-        self._x += 2
+        if 720 - 100 > self._y:
+            self._y += 4
 
-    def er_over_hinder(self):
-        pass
+
+
 
 class Hinder(Figur):
-    def __init__(self, x, y, fartX, hoyde, bredde):
+    def __init__(self, x, y, fartX, bredde):
         super().__init__(x, y)
         self._fartX = fartX
-        self._hoyde = hoyde
+        self._hoyde = randint(20, 600)
         self._bredde = bredde
+        self._y = 720 - self._hoyde
     
     def tegn(self, screen):
-        screen.blit(iskrem, (self._x,self._y))
+        screen.blit(iskrem, (self._x,self._y, self._bredde, self._hoyde))
 
     def flytt_venstre(self):
-        self._x += 1
+        self._x -= 2
 
 sirkel = Figur(30,100)
 spiller = Spiller(100,50,50)
 hindere = [] 
-for i in range(8): 
-    height = randint(20,100)
-    a = randint(10,100)
+m = randint(1,12)
+for i in range(m): 
     b = randint(10,200)
-    nytt_hinder = Hinder(a, b, 100, height, 10) 
+    nytt_hinder = Hinder(1270, b, 100, 10) 
     hindere.append(nytt_hinder) 
 
+
+
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
     # LAG SPILLET DIT HER:
 
     spiller.tegn()
-
     for hinder in hindere:
         hinder.flytt_venstre() 
         hinder.tegn(screen)
@@ -83,9 +94,24 @@ while running:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP]:
         spiller.hopp()
+    elif keys[pygame.K_LEFT]:
+        spiller.side_v()
+    elif keys[pygame.K_RIGHT]:
+        spiller.side_h()
     spiller.fall()
-    # flip() the display to put your work on screen
+
+ 
+    #telle tiden, poeng per sekund man overlever
+
+    score = 0
+    if spiller._y <= nytt_hinder._y and spiller._x <= nytt_hinder._x:
+        score -= 10
+    font = pygame.font.Font(None, 36)
+    score_text = font.render(f'Score: {score}', True, (255, 255, 255))
+    screen.blit(score_text, (10, 10))
+
+
     pygame.display.flip()
-    clock.tick(60)  # limits FPS to 60
+    clock.tick(60) 
 
 pygame.quit()
